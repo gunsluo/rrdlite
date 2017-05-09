c9e781eb08aa2053b80d309edd4f45ab5db78d09
jerrylou <gunsluo@gmail.com>
2017-05-09 17:46:35 +0800

    fix compile by windows
    Signed-off-by: jerrylou <gunsluo@gmail.com>

---
+++ rrd_c_windows.go
@@ -0,0 +1,21 @@
+/*
+ *Copyright 2016 yubo. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+package rrdlite
+
+import "strconv"
+
+func i64toa(i int64) string {
+	return strconv.FormatInt(i, 10)
+}
+
+func (c *Creator) create() error {
+	return nil
+}
+
+func (u *Updater) update(_args []string) error {
+
+	return nil
+}
